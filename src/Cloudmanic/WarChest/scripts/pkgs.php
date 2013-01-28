47e060cd41078e31b673e379ecbb17070d8a5c52
Cloudmanic Labs, LLC <github@cloudmanic.com>
2013-01-28 12:12:49 -0800

    added our custom package script

---
+++ src/Cloudmanic/WarChest/scripts/pkgs.php
@@ -0,0 +1,41 @@
+<?php
+//
+// Company: Cloudmanic Labs, LLC
+// Website: http://cloudmanic.com
+// Date: 11/28/2013
+//
+
+error_reporting(E_ALL);
+ini_set('display_errors', 'On');
+ini_set('display_startup_errors', 'On');
+
+// Change directories.
+chdir('../');
+
+// Get configs
+$config = require('pkg_list.php');
+$dir = $config['dir'];
+
+// Check to see if our package dir is created.
+if(! is_dir($dir))
+{
+	mkdir($dir);
+}
+
+// Loop through our repos and install or update.
+foreach($config['repos'] AS $key => $row)
+{
+	$name = str_ireplace('.git', '', basename($key));
+
+	// Check to see if we need to checkout or update.
+	if(! is_dir("$dir/$name"))
+	{
+		echo exec("cd $dir && git clone $key && git checkout $row");	
+	} else
+	{
+		echo exec("cd $dir/$name && git pull origin $row");
+	}
+}
+
+// We are done.
+echo "\nSuccess!!\n";
