18abade70585d191e5d4fe2f8246fe6c89023e9d
Cloudmanic Labs, LLC <github@cloudmanic.com>
2014-04-20 16:59:27 -0700

    Fixed bugs

--- src/Cloudmanic/WarChest/Models/BasicModel.php
+++ src/Cloudmanic/WarChest/Models/BasicModel.php
@@ -17,11 +17,12 @@ class BasicModel
 {	
 	public static $joins = null;
 	public static $_table = null;
-	public static $connection = 'mysql';
+	private static $_with = array();
+	public static $_connection = 'mysql';
 	protected static $query = null;
 	protected static $_extra = 0;
-	private static $_with = array();
 	protected static $_is_api = false;
+
 	
 	// ------------------------ Setters ------------------------------ //
 
@@ -34,29 +35,12 @@ class BasicModel
 	}
 
 	//
-	// Make it so it does not load the other models.
+	// Set connection.
 	//
-	public static function set_no_extra()
+	public static function set_connection($con)
 	{
-		self::$_extra = false;
+		self::$_connection = $con;
 	}
-	
-	//
-	// We want all the extra data.
-	//
-	public static function set_extra()
-	{
-		self::$_extra = true;
-	}
- 
- 	//
- 	// Set since a particular date.
- 	//
- 	public static function set_since($timestamp)
- 	{
-	 	$stamp = date('Y-m-d H:i:s', strtotime($timestamp));
-	 	self::get_query()->where(static::$_table . 'UpdatedAt', '>=', $stamp);
- 	}
 
 	//
 	// Set limit
@@ -83,19 +67,11 @@ class BasicModel
 	}	
 	
 	//
-	// Set group
-	//
-	public static function set_group($group)
-	{
-		self::get_query()->groupBy($group);
-	}	
-	
-	//
 	// Set Column.
 	//
-	public static function set_col($key, $value, $action = '=')
+	public static function set_col($key, $value)
 	{
-		self::get_query()->where($key, $action, $value);
+		self::get_query()->where($key, '=', $value);
 	}
 	
 	//
@@ -103,7 +79,7 @@ class BasicModel
 	//
 	public static function set_or_col($key, $value)
 	{
-		self::get_query()->orWhere($key, '=', $value);
+		self::get_query()->or_where($key, '=', $value);
 	}
 	
 	//
@@ -111,16 +87,8 @@ class BasicModel
 	//
 	public static function set_or_where_in($col, $list)
 	{
-		self::get_query()->orWhereIn($col, $list);
+		self::get_query()->or_where_in($col, $list);
 	}
-	
-	//
-	// Set Where In
-	//
-	public static function set_where_in($col, $list)
-	{
-		self::get_query()->whereIn($col, $list);
-	}	
 	
 	//
 	// Set Columns to select.
@@ -155,6 +123,14 @@ class BasicModel
 	}
 	
 	//
+	// Set extra
+	//
+	public static function set_extra($extra)
+	{
+		static::$_extra = $extra;
+	}	
+	
+	//
 	// Set search.
 	//
 	public static function set_search($str)
@@ -172,7 +148,7 @@ class BasicModel
 		$data = array();
 				
 		// Make sure we have a query started.
-		self::get_query();
+		static::get_query();
 		
 		// Do we have joins?
 		if(! is_null(static::$joins))
@@ -184,7 +160,7 @@ class BasicModel
 		}
 		
 		// Query
-		$data = self::get_query()->get();
+		$data = static::get_query()->get();
 		
 		// Convert to an array because we like arrays better.
 		$data = static::_obj_to_array($data);
@@ -192,22 +168,6 @@ class BasicModel
 		// Clear query.	
 		$table = static::$_table;
 		static::clear_query();
-		
-		// Remove any unwanted columns.
-		if(isset(static::$remove) && is_array(static::$remove))
-		{
-			// Loop through data and format.
-			foreach($data AS $key => $row)
-			{
-				foreach($row AS $key2 => $row2)
-				{
-					if(in_array($key2, static::$remove))
-					{
-						unset($data[$key][$key2]);
-					}
-				}
-			}
-		}
 		
 		// An option formatting function call.
 		if(method_exists($table, '_format_get'))
@@ -227,11 +187,11 @@ class BasicModel
 	// 
 	public static function get_by_id($id)
 	{
-		self::get_query();
-		self::set_col(self::$_table . 'Id', $id);
-		$d = self::get();
+		static::get_query();
+		static::set_col(static::$_table . 'Id', $id);
+		$d = static::get();
 		$data = (isset($d[0])) ? (array) $d[0] : false;
-		self::clear_query();		
+		static::clear_query();		
 		return $data;
 	}
 	
@@ -243,16 +203,16 @@ class BasicModel
 		// Make sure we have a query started.
 		self::get_query();
 	
-		// Add updated at date
- 		if(! isset($data[self::$_table . 'UpdatedAt'])) 
- 		{
- 			$data[self::$_table  . 'UpdatedAt'] = date('Y-m-d G:i:s');
- 		}
- 		
 		// Add created at date
  		if(! isset($data[self::$_table . 'CreatedAt'])) 
  		{
  			$data[self::$_table  . 'CreatedAt'] = date('Y-m-d G:i:s');
+ 		}
+ 		
+		// Add update at date
+ 		if(! isset($data[self::$_table . 'UpdatedAt'])) 
+ 		{
+ 			$data[self::$_table  . 'UpdatedAt'] = date('Y-m-d G:i:s');
  		}
 	
  		// Insert the data / clear the query and return the ID.
@@ -266,16 +226,14 @@ class BasicModel
 	//
 	public static function update($data, $id)
 	{	
-		// Add updated at date
+		// Add update at date
  		if(! isset($data[self::$_table . 'UpdatedAt'])) 
  		{
  			$data[self::$_table  . 'UpdatedAt'] = date('Y-m-d G:i:s');
  		}
 	
- 		// Run and clear query.
 		$rt = self::get_query()->where(self::$_table . 'Id', '=', $id)->update(self::_set_data($data));
 		self::clear_query();
-		
 		return $rt;
 	}	
 
@@ -306,9 +264,6 @@ class BasicModel
 	//
 	public static function get_count()
 	{
-		// Make sure we have a query started.
-		self::get_query();	
-	
 		// Do we have joins?
 		if(! is_null(static::$joins))
 		{
@@ -324,27 +279,6 @@ class BasicModel
 	}
 	
 	// ----------------- Helper Function  -------------- //
-		
- 	//
- 	// Convert the object the database returns to an array.
- 	// Yes, PDO can return arrays, but Laravel really counts
- 	// on objects instead of arrays.
- 	//
- 	private static function _obj_to_array($data)
- 	{
-	 	if(is_array($data) || is_object($data))
-	 	{
-		 	$result = array();
-		 	foreach($data as $key => $value)
-		 	{
-			 	$result[$key] = self::_obj_to_array($value);
-			}
-
-			return $result;
-		}
-    
-		return $data;
-	}
 	
 	//
 	// Get last query.
@@ -367,7 +301,7 @@ class BasicModel
 	//
 	// If we have a query already under way return it. If not 
 	// build the query and return a new object.
-	//	
+	//
 	protected static function get_query()
 	{
 		if(is_null(static::$query))
@@ -378,7 +312,7 @@ class BasicModel
 				static::$_table = end($table);			
 			}
 			
-			static::$query = DB::connection(static::$connection)->table(static::$_table);
+			static::$query = DB::connection(static::$_connection)->table(static::$_table);
 			return static::$query;
 		} else
 		{
@@ -392,7 +326,7 @@ class BasicModel
 	private static function _set_data($data)
  	{
  		$q = array();
- 		$fields = DB::connection(static::$connection)->select('SHOW COLUMNS FROM ' . static::$_table);
+ 		$fields = DB::connection(static::$_connection)->select('SHOW COLUMNS FROM ' . static::$_table);
  		
  		foreach($fields AS $key => $row)
  		{ 
@@ -404,6 +338,27 @@ class BasicModel
  		
  		return $q;
  	}
+ 	
+ 	//
+ 	// Convert the object the database returns to an array.
+ 	// Yes, PDO can return arrays, but Laravel really counts
+ 	// on objects instead of arrays.
+ 	//
+ 	private static function _obj_to_array($data)
+ 	{
+	 	if(is_array($data) || is_object($data))
+	 	{
+		 	$result = array();
+		 	foreach($data as $key => $value)
+		 	{
+			 	$result[$key] = self::_obj_to_array($value);
+			}
+
+			return $result;
+		}
+    
+		return $data;
+	}
 }
 
 /* End File */
